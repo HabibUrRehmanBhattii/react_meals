e3cc403054cdc4ff2721a892e6d500835d9cc68c
HabibUrRehmanBhattii <egalienv@gmail.com>
2022-11-12 09:49:53 +0100

    Add useEffect for custom css bump class for basket

--- src/dev/previews.jsx
+++ src/dev/previews.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import {PaletteTree} from './palette';
 import HeaderCartButton from "../components/Layout/HeaderCartButton";
+import CartProvider from "../store/CartProvider";
+import Meals from "../components/Meals/Meals";
 
 const ComponentPreviews = () => {
     return (
@@ -11,6 +13,12 @@ const ComponentPreviews = () => {
             </ComponentPreview>
             <ComponentPreview path="/HeaderCartButton">
                 <HeaderCartButton/>
+            </ComponentPreview>
+            <ComponentPreview path="/CartProvider">
+                <CartProvider/>
+            </ComponentPreview>
+            <ComponentPreview path="/Meals">
+                <Meals/>
             </ComponentPreview>
         </Previews>
     );
